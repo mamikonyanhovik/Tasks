89cf8b066c8cddd289d0212ac8b9bf598e06d4f4
Hovik <58630430+mamikonyanhovik@users.noreply.github.com>
2020-04-26 01:28:32 +0400

    Add files via upload

--- Obj test.js
+++ Obj test.js
@@ -81,7 +81,7 @@ function MakeCar (color) {
 }
 
 let c = new MakeCar('black');
-let c1 = new MakeCar //defaultov klni 'white'
+let c1 = new MakeCar //defaultov klni 'white'   
 c1.maxSpeed = "120km/h" //ete uzum enq arandzin property avelacnenq
 
 
@@ -104,3 +104,51 @@ let car2 = new constrictors.Car('black', 180);
 let dog1 = new constrictors.Dog('dog', 'male');
 let dog2 = new constrictors.Dog('doberman', 'female');
 
+//// եթե ֆունկցիան գրելուց new օպերատորը չի օգտագործվել, ես դեպքում ծրագիրը ավտոմատ կավելացնի
+
+function A() {
+  if(!new.target) {
+    return new A()
+  }
+}
+
+//metodner avelacnel
+function Car (model, year, color) {
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.speed = 0;
+  this.go = function(speed) {
+    this.speed = speed;
+  }
+  this.stop = function() {
+    this.speed = 0;
+  } 
+  this.changeColor = (color) => {
+    this.color = color;
+  }
+
+}
+
+let bmw = new Car("BMW e60", 2005, "white");
+let toyota = new Car("Toyota Camry", 2019, 'black');
+toyota.go(100); //aragutyun@ vor@ functia er, sarqecinq 100
+toyota.stop //speed@ sarquma 0
+toyota.changeColor("red"); //arrow functioni depqum drsi this-na vekalum, color@ sarquma red;
+//instance stugel@
+toyota instanceof Car; //true
+
+// urish tarberak
+Car = function (model) {
+  this.model = model;
+  this.speed = 0;
+  this.go = function(speed) {
+    this.speed = speed;
+  }
+  this.stop = function() {
+    this.speed = 0;
+  } 
+}
+
+let t = new Car("Camry");
+let b = new Car("7");
